2c070fe4246cad11bb8cc57c599d97e9610f5191
SauravMuke54 <saurav54muke@gmail.com>
2022-12-14 17:11:05 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+
+const app = express()
+
+require('dotenv').config()
+
+const port = 4000
+
+const authRoutes=require('./routes/auth')
+const userRoutes=require('./routes/user')
+
+const mongoose = require('mongoose');
+//Set up default mongoose connection
+
+const bodyParser=require('body-parser')
+const cookieParser =require('cookie-parser')
+const cors=require('cors')
+
+app.use(express.static("public"))
+
+//app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+mongoose.set('strictQuery', true);
+//Database connection
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+console.log("DB Connected");
+});
+
+//using middlewares 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api",authRoutes);
+app.use("/api",userRoutes);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
